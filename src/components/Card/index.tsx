0efa6820036775c6c7c938dfb6e3a4c272e30724
Alexei Ilinykh <alexeilinykh@gmail.com>
2025-08-04 16:49:46 -0700

    Don't show div if there's no image

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -39,7 +39,11 @@ export const Card: React.FC<{
     >
       <div className="relative w-full ">
         {!metaImage && <div className="">No image</div>}
-        {metaImage && typeof metaImage !== 'string' && <Media resource={metaImage} size="33vw" />}
+        {metaImage && typeof metaImage !== 'string' && (
+          <div className="relative w-full ">
+            <Media resource={metaImage} size="33vw" />
+          </div>
+        )}
       </div>
       <div className="p-4">
         {showCategories && hasCategories && (
