b00e33fbebd955ce429ef86e8fd123bab9645269
Alexei Ilinykh <alexeilinykh@gmail.com>
2025-08-04 15:45:06 -0700

    Update logo

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -32,9 +32,7 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
   return (
     <header className="container relative z-20   " {...(theme ? { 'data-theme': theme } : {})}>
       <div className="py-8 flex justify-between">
-        <Link href="/">
-          <Logo loading="eager" priority="high" className="invert dark:invert-0" />
-        </Link>
+        <Link href="/">TechBriefAI</Link>
         <HeaderNav data={data} />
       </div>
     </header>
