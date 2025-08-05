6afc1a5307aa367f34e6e1e7df5b09a35b4ce3bc
Alexei Ilinykh <alexeilinykh@gmail.com>
2025-08-04 20:44:13 -0700

    Remove date label

--- src/heros/PostHero/index.tsx
+++ src/heros/PostHero/index.tsx
@@ -54,8 +54,6 @@ export const PostHero: React.FC<{
             )}
             {publishedAt && (
               <div className="flex flex-col gap-1">
-                <p className="text-sm">Date Published</p>
-
                 <time dateTime={publishedAt}>{formatDateTime(publishedAt)}</time>
               </div>
             )}
