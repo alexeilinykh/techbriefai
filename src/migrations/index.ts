87fac3ed6b1b521e674a98ccb4ec59bb2b754e90
Alexei Ilinykh <alexeilinykh@gmail.com>
2025-09-09 22:11:15 -0700

    Remove verification

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
-import * as migration_20250910_043145 from './20250910_043145'
+import * as migration_20250910_043145 from './20250910_043145';
+import * as migration_20250910_051049 from './20250910_051049';
 
 export const migrations = [
   {
@@ -6,4 +7,9 @@ export const migrations = [
     down: migration_20250910_043145.down,
     name: '20250910_043145',
   },
-]
+  {
+    up: migration_20250910_051049.up,
+    down: migration_20250910_051049.down,
+    name: '20250910_051049'
+  },
+];
