f48509c0970ed2a71efb56d807fa5af7e68945bd
Alexei Ilinykh <alexeilinykh@gmail.com>
2025-09-09 20:34:18 -0700

    Undo migrations

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20250910_023051 from './20250910_023051';
-
-export const migrations = [
-  {
-    up: migration_20250910_023051.up,
-    down: migration_20250910_023051.down,
-    name: '20250910_023051'
-  },
-];
