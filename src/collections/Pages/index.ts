720d964f8e7f35bd99ae1f7fc87518faf681f755
Alexei Ilinykh <alexeilinykh@gmail.com>
2025-10-20 19:46:59 -0700

    30 second autosave on pages collection

--- src/collections/Pages/index.ts
+++ src/collections/Pages/index.ts
@@ -130,7 +130,7 @@ export const Pages: CollectionConfig<'pages'> = {
   versions: {
     drafts: {
       autosave: {
-        interval: 100, // We set this interval for optimal live preview
+        interval: 30000, // We set this interval for optimal live preview
       },
       schedulePublish: true,
     },
